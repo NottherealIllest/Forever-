bc6a5f389bbe0d3f7171add825600eca0d1fcf5e
Silas <silasfaarda@gmail.com>
2021-03-16 20:46:06 +0100

    habeeb don suffer me

--- js/button.js
+++ js/button.js
@@ -16,8 +16,8 @@ audio.addEventListener("loadedmetadata", (e) => {
         if (audio.paused) {
             audio.play();
             buttonIcon.innerHTML = `
-      <rect width="5" height="38" fill="#000"/>
-      <rect x="14" width="5" height="38" fill="#000"/>
+      <rect width="5" height="38" fill="#ccc"/>
+      <rect x="14" width="5" height="38" fill="#ccc"/>
       `;
             buttonIcon.setAttribute("viewBox", "0 0 19 38");
             buttonIcon.setAttribute("height", "24");
@@ -26,7 +26,7 @@ audio.addEventListener("loadedmetadata", (e) => {
         } else {
             audio.pause();
             buttonIcon.innerHTML = `
-      <path d="M19.1293 14.4571C20.0992 15.2571 20.0992 16.7429 19.1293 17.5429L3.27257 30.6213C1.96809 31.6973 0 30.7694 0 29.0784L0 2.92157C0 1.23063 1.96809 0.302743 3.27257 1.37866L19.1293 14.4571Z" fill="black"/>
+      <path d="M19.1293 14.4571C20.0992 15.2571 20.0992 16.7429 19.1293 17.5429L3.27257 30.6213C1.96809 31.6973 0 30.7694 0 29.0784L0 2.92157C0 1.23063 1.96809 0.302743 3.27257 1.37866L19.1293 14.4571Z" fill="#ccc"/>
       `;
             buttonIcon.setAttribute("viewBox", "0 0 20 32");
             buttonIcon.setAttribute("height", "19.2");
